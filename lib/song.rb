087f631d30b4640123f2b547350c0314896c214c
Tim Quimbo <tquimbo1@gmail.com>
2021-03-09 03:23:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ def self.genre_count
 #if it has increment key by 1, if not create a new key value pair
 
 genre_count = {}
-@@genre.each do |genre, count|
+@@genres.each do |genre, count|
 genre_count << genre[count]
 end
 end
